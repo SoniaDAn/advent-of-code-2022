fbc7c6308533d51a40820b5a5328203f805f9d4b
Sonia DAndreta <sonia.dandreta@autotrader.co.uk>
2025-02-20 13:42:18 +0000

    Removed unused imports

--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -2,9 +2,6 @@ package aoc.day01;
 
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
