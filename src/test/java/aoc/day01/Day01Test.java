7467fc68cb2505d82167d52c822e1432c7fc5dde
Sonia DAndreta <sonia.dandreta@autotrader.co.uk>
2025-02-20 13:41:33 +0000

    Removed defunct test

--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -12,18 +12,6 @@ import static org.assertj.core.api.Assertions.*;
 public class Day01Test {
 
     @Test
-    public void testPart2(){
-        // Given
-        List<String> input = Collections.singletonList("test");
-
-        // When
-        String result = new Day01().part2(input);
-
-        // Then
-        assertThat(result).isEqualTo(input.get(0));
-    }
-
-    @Test
     public void convertsToInt_convertsStringToInteger() {
         // Given
         String line = "1234";
